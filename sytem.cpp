d87c171c50269cdffac1f05a075294b60c8ce003
hipsty1 <123240066@student.upnyk.ac.id>
2025-03-01 19:21:03 +0700

    perbaikan sistem dari awal, dan tambahan fungsi reserch, btw ni rekursip apaan ya bang hehe

--- sytem.cpp
+++ sytem.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 #include <string>
 using namespace std;
 
 // deklarasi
-int saldo = 0, pilihTopUp;
-char pilih;
-string username, password, passwordCheck;
+int saldo = 0;
+string namaUser, passUser;
+//int pilihTopUp;
+//string passwordCheck;
 
 // data
+// data user
+struct Account { 
+    string username;
+    string password;
+};
+Account account [10] = {
+    {"Hendri","123240066"},
+    {"Restu","123240050"},
+    {"Zen","123240061"},
+    {"Deva","123240080"}};
+
+/*
 // data nama stand
 string kantin[5] = {"Dapur Mak'E - Depok", "Kentang Kletji", "Soto Seger Boyolali - Bu Retno", "Komunitas Kantin Puspita", "Kantin 21"};
 // data menu kantin
@@ -23,37 +37,142 @@ menu kantin4[6] = {{"Indomie Telur", 10000}, {"Nasi Rawon", 16000}, {"Nasi Ayam 
 menu kantin5[8] = {{"Indomie Goreng Kornet", 10000}, {"Indomie rebus Kornet", 10000}, {"Indomie Goreng Kornet Telur", 13000}, {"Indomie Rebus Kornet", 13000}, {"Indomie Goreng Kornet Sosis", 9000}, {"Indomie Rebus Kornet", 9000}, {"Es Jeruk", 3000}, {"Es Nutrisari", 4000}};
 // data menu top up
 int topUp[6] = {5000, 10000, 20000, 25000, 50000, 100000};
-
+*/
 // prototype
-void login();
+void firstMenu();
+void signup();
+void signin();
 void profile();
-void tambahSaldo();
-void buy();
-void exit();
+// void tambahSaldo();
+// void buy();
+// void exit();
 
 int main(){
-    login();
-    profile();
+    firstMenu();
     return 0;
 }
 
-void login(){
+void firstMenu(){
+    int pilih;
+    // tampilan awal
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
-    cout << setfill(' ') << setw(17) << " " << "LOGIN" << endl;
+    cout << setfill(' ') << setw(15) << " " << "MENU AWAL" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
-    cout << "Masukkan Username : "; getline(cin, username);
-    cout << "Masukkan Password : "; getline(cin, password);
+    cout << "1. Sign Up" << endl << "2. Sign In" << endl << "3. Out" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
+    cout << "Pilih : "; cin >> pilih;
     system("pause");
+
+    system("cls");
+    switch(pilih){
+        case 1:
+        signup();
+        break;
+
+        case 2:
+        signin();
+        break;
+
+        case 3:
+        exit(0);
+        break;
+
+        default:
+        firstMenu();
+    }
 }
 
+//menu sig in (daftar account)
+void signup(){
+    Account newAccount;
+    cout << setfill('=') << setw(40) << "=" << endl;
+    cout << setfill(' ') << setw(17) << " " << "SIGNUP" << endl;
+    cout << setfill('=') << setw(40) << "=" << endl;
+    cout << "Masukkan Username (tanpa spasi) : "; cin >> newAccount.username;
+    cout << "Masukkan Password (gunakan NIM) : "; cin >> newAccount.password;
+
+    // Cek apakah username sudah ada
+    for(int i = 0; i < 10; i++) {
+        if(account[i].username == newAccount.username) {
+            cout << setfill('=') << setw(40) << "=" << endl;
+            cout << "Username sudah digunakan. Coba lagi." << endl;
+            cout << setfill('=') << setw(40) << "=" << endl;
+            system("pause");
+            return signup();
+        }
+    }
+
+    // Menyimpan akun baru ke dalam array jika masih ada slot kosong
+    for(int i = 0; i < 10; i++) {
+        if(account[i].username.empty()) {  
+            account[i] = newAccount;
+            namaUser = newAccount.username;
+            passUser = newAccount.password;
+            cout << setfill('=') << setw(40) << "=" << endl;
+            cout << "Akun berhasil dibuat!" << endl;
+            cout << setfill('=') << setw(40) << "=" << endl;
+            system("pause");
+            profile();
+            return;
+        }
+    }
+    cout << setfill('=') << setw(40) << "=" << endl;
+    cout << "Pendaftaran gagal! Kapasitas akun penuh." << endl;
+    cout << setfill('=') << setw(40) << "=" << endl;
+    system("pause");
+    firstMenu();
+}
+
+// menu signin
+void signin() {
+    string username, password;
+    bool cek = false; // Menyimpan status login berhasil/gagal
+
+    cout << setfill('=') << setw(40) << "=" << endl;
+    cout << setfill(' ') << setw(17) << " " << "SIGN IN" << endl;
+    cout << setfill('=') << setw(40) << "=" << endl;
+    cout << "Masukkan Username : "; cin >> username;
+    cout << "Masukkan Password : "; cin >> password;
+    cout << setfill('=') << setw(40) << "=" << endl;
+
+    // Perbaikan: Cek hanya dalam batas jumlah akun yang tersedia
+    int jumlahAkun = sizeof(account) / sizeof(account[0]);
+    
+    for (int i = 0; i < jumlahAkun; i++) {
+        if (account[i].username == username && account[i].password == password) {
+            cek = true;
+            break; // Keluar dari loop jika sudah ditemukan akun yang cocok
+        }
+    }
+
+    if (cek) {
+        namaUser = username;
+        passUser = password;
+        system("cls");
+        cout << setfill('=') << setw(40) << "=" << endl;
+        cout << setfill(' ') << setw(14) << " " << "SIGN IN SUCCESS" << endl;
+        cout << setfill('=') << setw(40) << "=" << endl;
+        system("pause");
+        profile(); // Masuk ke menu profile setelah login berhasil
+    } else {
+        system("cls");
+        cout << setfill('=') << setw(40) << "=" << endl;
+        cout << setfill(' ') << setw(14) << " " << "SIGN IN FAILED" << endl;
+        cout << setfill('=') << setw(40) << "=" << endl;
+        system("pause");
+        signin(); // Meminta user login kembali jika gagal
+    }
+}
+
+
 void profile(){
+    char pilih;
     system("cls");
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << setfill(' ') << setw(16) << " " << "PROFILE" << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
-    cout << "NAME\t: " << username << endl;
+    cout << "NAME\t: " << namaUser << endl;
     cout << "SALDO\t: " << saldo << endl;
     cout << setfill('=') << setw(40) << "=" << endl;
     cout << "1. TOP UP" << endl;
@@ -68,15 +187,15 @@ void profile(){
     system("pause");
     switch (pilih){
     case '1':
-        tambahSaldo();
+        // tambahSaldo();
         break;
 
     case '2':
-        buy();
+        // buy();
         break;
 
     case '3':
-        exit(0);
+        // exit(0);
         break;
 
     default:
@@ -89,6 +208,8 @@ void profile(){
         profile();
     }
 }
+
+/*
 
 void tambahSaldo()
 {
@@ -207,3 +328,4 @@ void exit(){
     exit(0);
     
 }
+*/
